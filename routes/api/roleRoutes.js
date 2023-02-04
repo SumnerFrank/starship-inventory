9a2698b1c4574f7cce4a285586382f7df5ddacb6
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 16:36:19 -0500

    added initial routes to role

--- routes/api/roleRoutes.js
+++ routes/api/roleRoutes.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../../config/connection');
+
+//get roles from db
+router.get('role', (req,res) => {
+    const sql = `SELECT * FROM role`;
+});
+
+//add role to db
+router.post('role', (req,res) => {
+    const sql = `INSERT INTO role`;
+});
+
+//get individual role from db
+router.get('/role/:id', (req,res) => {
+    const sql = `SELECT * FROM role`;
+});
+
+module.exports = router;
