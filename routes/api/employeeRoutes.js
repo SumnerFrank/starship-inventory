f1ae4f5ed6212a02027d65d431b0fa711a339a65
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 16:35:04 -0500

    added initial routes emplooyee

--- routes/api/employeeRoutes.js
+++ routes/api/employeeRoutes.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../../config/connection');
+
+//get e,ployees from db
+router.get('employee', (req,res) => {
+    const sql = `SELECT * FROM employee`;
+});
+
+//add employee to db
+router.post('employee', (req,res) => {
+    const sql = `INSERT INTO employee`;
+});
+
+//get individual employee from db
+router.get('/employee/:id', (req,res) => {
+    const sql = `SELECT * FROM employee`;
+});
+
+module.exports = router;
