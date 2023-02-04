05c201b5b42822a485e05277f5b29df7f3bbf19c
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 15:50:25 -0500

    began router code

--- routes/api/departmentRoutes.js
+++ routes/api/departmentRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const db = require('../../config/connection');
 const router = express.Router();
 const config = require('../../config/connection');
 
 
+//get departments from db
+router.get('department', (req,res) => {
+    const sql = `SELECT * FROM department`;
+})
 
-
+//add department to db
+router.post('department', (req,res) => {
+    const sql = `INSERT INTO department`;
+})
 
 module.exports = router;
