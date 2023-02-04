02591a919e2bf60fc030ea6e553dd397a1101295
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 07:56:45 -0500

    template for initial prompt and directory

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const config = require('./config/connection')
 
+const startPrompt = () => {
+    inquirer.prompt([
+        // {initial prompt, directory?}
+    ]).then(employerData => {
+        // if else statements based on choices to initial prompt
+    })
+}
