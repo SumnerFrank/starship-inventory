cbf9e2c8dd2a85c487f1c781b53c0485be904564
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 08:19:05 -0500

    added connection to database

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        user: 'user',
+        password: 'password',
+        database: ''
+    },
+    console.log('Successfully connected to ____ database.')
+); 
