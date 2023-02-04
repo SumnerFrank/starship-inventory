f4c115faddf0e061f878a7996fbe72d6c62904bc
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 09:27:16 -0500

    initial attempt at server code

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const config = require('./config/connection');
+
+// const routes = routes need to be made still 
+
+//middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// app.use('/api', routes)
+
+//404 response
+app.use((req,res) => {
+    res.status(404).end();
+});
+
+//function needed to start server
