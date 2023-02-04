78999b2790fcd6f46fb3ac4704a53696a0aa2c1b
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-04 16:16:21 -0500

    added get for individual departments

--- routes/api/departmentRoutes.js
+++ routes/api/departmentRoutes.js
@@ -7,11 +7,16 @@ const config = require('../../config/connection');
 //get departments from db
 router.get('department', (req,res) => {
     const sql = `SELECT * FROM department`;
-})
+});
 
 //add department to db
 router.post('department', (req,res) => {
     const sql = `INSERT INTO department`;
-})
+});
+
+//get individual department from db
+router.get('/department/:id', (req,res) => {
+    const sql = `SELECT * FROM department`;
+});
 
 module.exports = router;
