e33319189be23ecdb88071dac5c057980b68719f
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-07 08:41:44 -0500

    started prompt lines

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const startPrompt = () => {
     inquirer.prompt([
         // {initial prompt, directory?}
         // would want to view/add departments, roles, and employees
+
+        {
+            type: 'list',
+            name: 'options', 
+            message: 'What would you like to do',
+            choices: ['', '', '', '', '', '', '', '']
+        }
     ]).then(employerData => {
         // if else statements based on choices to initial prompt
     });
