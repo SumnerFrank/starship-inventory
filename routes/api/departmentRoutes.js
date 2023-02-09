6d7d1d0460c95a74f3a443d03ee2deb914ea3835
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-09 08:38:52 -0500

    udpated routes

--- routes/api/departmentRoutes.js
+++ routes/api/departmentRoutes.js
@@ -4,17 +4,21 @@ const router = express.Router();
 
 
 //get departments from db
-router.get('department', (req,res) => {
+router.get('/department', (req,res) => {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({error: err.message});
         }
+        res.json({
+            message: 'Success!',
+            data: rows
+        });
     })
 });
 
 //add department to db
-router.post('department', (req,res) => {
+router.post('/department', (req,res) => {
     const sql = `INSERT INTO department`;
 
 });
@@ -22,6 +26,16 @@ router.post('department', (req,res) => {
 //get individual department from db
 router.get('/department/:id', (req,res) => {
     const sql = `SELECT * FROM department`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({error: err.message});
+        }
+        res.json({
+            message: 'Success!',
+            data: rows
+        });
+    });
 });
 
 module.exports = router;
